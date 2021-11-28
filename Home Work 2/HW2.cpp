35c0288cf4c53d782ff3821b60d8211ec0181518
WePlayFleX <alex.kamai@ya.ru>
2021-11-29 00:15:07 +0300

    Добавьте файлы проекта.

---
+++ Home Work 2/HW2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " In the summer it is hot.\n";
+	cout << " \t In the winter it is not.\n";
+	cout << " \t \t In the spring there are flowers.\n";
+	cout << " \t \t \t In the autumn there are showers.\n";
+	system("pause");
+	return 0;
+}
